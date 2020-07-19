794d71c59b419884ca6ff7c3651088a1d99d5932
rossning92 <rossning92@gmail.com>
2020-07-18 17:07:04 -0700

    Add tests for multiply function.

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 int multiply(int a, int b)
 {
@@ -10,8 +11,15 @@ int divide(int a, int b)
     return a / b;
 }
 
+int test()
+{
+    assert(multiply(2, 2) == 4);
+    assert(multiply(-3, 2) == 6);
+}
+
 int main()
 {
     printf("Yoyo, Little Ross!");
+    test();
     return 0;
 }
